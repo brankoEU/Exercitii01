3dd61c2552c6ccf9695c63976367189bc7897aba
brankoEU <30345282+brankoEU@users.noreply.github.com>
2017-07-24 09:12:45 +0300

    Initial commit

---
+++ Goats/Goats/GoatsTest.cs
@@ -0,0 +1,34 @@
+﻿
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Goats
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            float HayPerGoat = KgHayPerGoatPerDay(18, 2, 5);
+            Assert.AreEqual(1.8f, HayPerGoat);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            float HayPerGoat = KgHayPerGoatPerDay(18, 2, 6);
+            float total = KgTotal(3, 5, HayPerGoat);
+            Assert.AreEqual(22.5f, total);
+        }
+
+        float KgHayPerGoatPerDay(float hayZ, int goatsY, int daysX)
+        {
+            return hayZ / goatsY / daysX;
+        }
+        float KgTotal(int goatsQ, int daysW,float KgPerGoat)
+        {
+            return KgPerGoat * goatsQ * daysW;
+        }
+
+    }
+}
