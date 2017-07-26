c4b7d25eb1d9fd31f36543299396b07693f4890c
brankoEU <30345282+brankoEU@users.noreply.github.com>
2017-07-26 17:15:24 +0300

    Mushrooms

---
+++ mushrooms/mushrooms/MushroomsTests.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace mushrooms
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int redMushrooms = CalculateRedMushrooms(3, 2);
+            Assert.AreEqual(2, redMushrooms);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            int redMushrooms = CalculateRedMushrooms(12, 3);
+            Assert.AreEqual(9, redMushrooms);
+        }
+
+        int CalculateRedMushrooms(int TotalMushrooms, int difference)
+        {
+            int whiteMushrooms = TotalMushrooms / (1 + difference);
+            return whiteMushrooms*difference;
+        }
+    }
+}
