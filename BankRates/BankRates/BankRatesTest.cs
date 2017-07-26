1e7bf09ba5418ac65d7b4641258a7fd2b0e3ad3b
brankoEU <30345282+brankoEU@users.noreply.github.com>
2017-07-27 00:00:07 +0300

    basic rate formula

--- BankRates/BankRates/BankRatesTest.cs
+++ BankRates/BankRates/BankRatesTest.cs
@@ -14,9 +14,26 @@ namespace BankRates
             Assert.AreEqual(51,rate);
         }
 
+        [TestMethod]
+        public void RateForSecondMonth()
+        {
+            decimal rate = CalculateBankRate(100, 2, 12, 2);
+            Assert.AreEqual(50.5m, rate);
+        }
+
+        [TestMethod]
+        public void RateForSevenMonth()
+        {
+            decimal rate = CalculateBankRate(100, 10, 12, 7);
+            Assert.AreEqual(10.4m, rate);
+        }
+
         decimal CalculateBankRate(decimal total, int periodInMonths, decimal interestPerYear, int currentMonth)
         {
-            return total/periodInMonths+total * interestPerYear/12/100;
+            decimal interestPerMonth = interestPerYear / 12 / 100;
+            decimal basePerMounth = total / periodInMonths;
+            decimal sold = total - (currentMonth - 1) * basePerMounth;
+            return basePerMounth + sold * interestPerMonth;
         }
     }
 }
