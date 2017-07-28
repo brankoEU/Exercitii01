6400dab3e2c03d8c558b0388d1376cc77b3b380e
brankoEU <30345282+brankoEU@users.noreply.github.com>
2017-07-28 12:38:13 +0300

    Exercitiu Sportiv

---
+++ Workouts/Workouts/SportWo.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Workouts
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int totalRepetitions = CalculateWorkouts(3);
+            Assert.AreEqual(9, totalRepetitions);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            int totalRepetitions = CalculateWorkouts(9);
+            Assert.AreEqual(81, totalRepetitions);
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+            int totalRepetitions = CalculateWorkouts(12);
+            Assert.AreEqual(144, totalRepetitions);
+        }
+
+        int CalculateWorkouts(int series)
+        {
+            return series*series;
+        }
+    }
+}
