a338e5f266af8f2e1f3c3f80e76816f5b59e65ab
brankoEU <30345282+brankoEU@users.noreply.github.com>
2017-07-31 13:09:34 +0300

    test2fail

---
+++ Squares/Squares/SquareCobblestonesTest.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Squares
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int totalCobblestones = CalcutateCobblestones(4, 3, 1);
+            Assert.AreEqual(12, totalCobblestones);
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            int totalCobblestones = CalcutateCobblestones(7, 5, 2);
+            Assert.AreEqual(9, totalCobblestones);
+        }
+
+        int CalcutateCobblestones(int squareLength, int squareWidth, int cobblestoneSide)
+        {
+            int squareArea = squareLength * squareWidth;
+            int cobblestoneArea = cobblestoneSide * cobblestoneSide;
+            double cobblestones = squareArea / cobblestoneArea;
+            return (int)Math.Ceiling(cobblestones);
+        }
+    }
+}
