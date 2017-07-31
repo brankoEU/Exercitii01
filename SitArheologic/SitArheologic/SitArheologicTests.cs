d0eb90fac28bb6b6e9ba31b50931ea8f8df11562
brankoEU <30345282+brankoEU@users.noreply.github.com>
2017-07-31 20:12:48 +0300

    commit final sit arheologic

--- SitArheologic/SitArheologic/SitArheologicTests.cs
+++ SitArheologic/SitArheologic/SitArheologicTests.cs
@@ -10,14 +10,22 @@ namespace SitArheologic
         [TestMethod]
         public void TestMethod1()
         {
-            float buildingArea = CalculateMinimalArea(2, 1, 4, 2, 5, 1);
-            Assert.AreEqual(6, buildingArea);
+            double buildingArea = CalculateMinimalArea(2, 1, 4, 2, 5, 1);
+            Assert.AreEqual(1.5d, buildingArea);
         }
 
-        float CalculateMinimalArea(float x1, float y1, float x2, float y2, float x3, float y3)
+        [TestMethod]
+        public void TestMethod2()
         {
-            float triangleArea = x1 * y2 + x2 * y3 + x3 * y1 - x3 * y2 - x1 * y3 - x2 * y1;
-            return Math.Abs(triangleArea * 2);
+            double buildingArea = CalculateMinimalArea(2.124587, 1.567542, 4.987512, 2.654821, 5.248657, 1.124589);
+            Assert.AreEqual(2.332438, buildingArea);
+        }
+
+        double CalculateMinimalArea(double x1, double y1, double x2, double y2, double x3, double y3)
+        {
+            double determinant = x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2);
+            double triangleArea = Math.Abs(determinant / 2);
+            return Math.Round(triangleArea, 6);
         }
     }
 }
