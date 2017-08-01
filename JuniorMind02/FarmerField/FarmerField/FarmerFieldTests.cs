a6dd1123b4ccdc6d08af6bfad3d42fe7bd717c25
brankoEU <30345282+brankoEU@users.noreply.github.com>
2017-08-01 12:18:32 +0300

    Fermier Commit2

--- JuniorMind02/FarmerField/FarmerField/FarmerFieldTests.cs
+++ JuniorMind02/FarmerField/FarmerField/FarmerFieldTests.cs
@@ -13,9 +13,24 @@ namespace FarmerField
             Assert.AreEqual(2, CalculateInitialFieldSize(1, 6));
         }
 
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.AreEqual(770, CalculateInitialFieldSize(230, 770000));
+        }
+
+        double CalculEcuatie(int b, int c)
+        {
+            double delta = Math.Pow(b, 2) + 4 * c;
+            double x1 = (-b + Math.Sqrt(delta)) / 2;
+            double x2 = (-b - Math.Sqrt(delta)) / 2;
+            return x1 > 0 ? x1 : x2;
+        }
+
         double CalculateInitialFieldSize(int fieldWidth, int fieldTotalSize)
         {
-            return 0;
+            double fieldLenght = CalculEcuatie(fieldWidth, fieldTotalSize);
+            return fieldLenght;
         }
     }
 }
