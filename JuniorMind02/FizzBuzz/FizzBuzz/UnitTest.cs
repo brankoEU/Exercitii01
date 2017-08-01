f494aac5649636e557d7adcc81f17974630b0d2f
brankoEU <30345282+brankoEU@users.noreply.github.com>
2017-08-01 13:08:15 +0300

    FizzBuzz commit2

--- JuniorMind02/FizzBuzz/FizzBuzz/UnitTest.cs
+++ JuniorMind02/FizzBuzz/FizzBuzz/UnitTest.cs
@@ -13,9 +13,28 @@ namespace FizzBuzz
             Assert.AreEqual("Fizz", FizzBuzz(3));
         }
 
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.AreEqual("Buzz", FizzBuzz(5));
+        }
+
         string FizzBuzz(int number)
         {
-            return "";
+            int aux = 0;
+            if (number % 3 == 0) { aux = 1; }
+            else
+            {
+                if (number % 5 == 0) { aux = 2; }
+                else { if (number % 3 == 0 || number % 5 == 0) { aux = 3; } }
+            }
+            switch(aux)
+                {
+                    case 1: return "Fizz";
+                    case 2: return "Buzz";
+                    case 3: return "FizzBuzz";
+                    default: return "Numarul nu este divizibil prin 3 sau 5!";
+                }
         }
     }
 }
