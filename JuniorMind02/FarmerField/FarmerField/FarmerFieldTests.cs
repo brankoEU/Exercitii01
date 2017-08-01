364047725d9cf9cd66f9e6a57b2aae520839d571
brankoEU <30345282+brankoEU@users.noreply.github.com>
2017-08-01 10:35:43 +0300

    problema fermier commit 1

---
+++ JuniorMind02/FarmerField/FarmerField/FarmerFieldTests.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FarmerField
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(2, CalculateInitialFieldSize(1, 6));
+        }
+
+        double CalculateInitialFieldSize(int fieldWidth, int fieldTotalSize)
+        {
+            return 0;
+        }
+    }
+}
