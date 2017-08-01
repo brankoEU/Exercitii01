897515692908a7aabd7f26d5aa61fa399038ddf4
brankoEU <30345282+brankoEU@users.noreply.github.com>
2017-08-01 12:44:12 +0300

    FizzBuzz commit1

---
+++ JuniorMind02/FizzBuzz/FizzBuzz/UnitTest.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FizzBuzz
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual("Fizz", FizzBuzz(3));
+        }
+
+        string FizzBuzz(int number)
+        {
+            return "";
+        }
+    }
+}
