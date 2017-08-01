4224da540dfe8b67ecd73c12100efa26c004aef2
brankoEU <30345282+brankoEU@users.noreply.github.com>
2017-08-01 09:51:58 +0300

    Melon commit1

---
+++ JuniorMind02/Melone/Melone/MelonTests.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Melone
+{
+    [TestClass]
+    public class MelonTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual("DA", DivideMelon(4));
+        }
+
+        string DivideMelon(int melonWeight)
+        {
+            return "NU";
+        }
+    }
+}
