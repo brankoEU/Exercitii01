76f10fc1534bd10d848a5a497df549229c48ebad
brankoEU <30345282+brankoEU@users.noreply.github.com>
2017-08-01 19:56:38 +0300

    RentDebt commit1

---
+++ JuniorMind02/RentDebt/RentDebt/RentDebtTests.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RentDebt
+{
+    [TestClass]
+    public class RentDebtTests
+    {
+        [TestMethod]
+        public void Delay5Days()
+        {
+            Assert.AreEqual(11, CalculateRent(10, 5));
+        }
+
+        decimal CalculateRent(decimal rent, int daysDelay)
+        {
+            return 0;
+        }
+    }
+}
