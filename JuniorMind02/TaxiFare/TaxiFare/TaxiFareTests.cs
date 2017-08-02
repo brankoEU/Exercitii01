6ec4d69c99857ac16e51cf0b92ec4a0e35c6ab21
brankoEU <30345282+brankoEU@users.noreply.github.com>
2017-08-02 11:57:25 +0300

    commit 3
    adaugat preturi in sir  si test distante mari

--- JuniorMind02/TaxiFare/TaxiFare/TaxiFareTests.cs
+++ JuniorMind02/TaxiFare/TaxiFare/TaxiFareTests.cs
@@ -19,11 +19,19 @@ namespace TaxiFare
             Assert.AreEqual(240, CalculateTaxiFare(30, 9));
         }
 
+        [TestMethod]
+        public void TestDayTimeLongDistance()
+        {
+            Assert.AreEqual(600, CalculateTaxiFare(100, 9));
+        }
+
         decimal CalculateTaxiFare(int numberKm, int hour)
         {
+            decimal[] DayTimePrices = { 5, 8, 6 };
             decimal pricePerKm = 0;
-            if (numberKm >= 21 && numberKm <= 60) pricePerKm = 8;
-            else pricePerKm = 5;
+            if (numberKm >= 21 && numberKm <= 60) pricePerKm = DayTimePrices[1];
+            else if (numberKm >= 61) pricePerKm = DayTimePrices[2];
+            else pricePerKm = DayTimePrices[0];
             return numberKm * pricePerKm;
         }
     }
