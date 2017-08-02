ac95b003a4225e68dbac16f45f75e266ca2f521e
brankoEU <30345282+brankoEU@users.noreply.github.com>
2017-08-02 10:42:34 +0300

    TaxiFare commit1
    Test 1 pass

---
+++ JuniorMind02/TaxiFare/TaxiFare/TaxiFareTests.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TaxiFare
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestDayTimeShortDistance()
+        {
+            Assert.AreEqual(25, CalculateTaxiFare(5, 9));
+        }
+
+        decimal CalculateTaxiFare(int numberKm, int hour)
+        {
+            return numberKm * 5;
+        }
+    }
+}
