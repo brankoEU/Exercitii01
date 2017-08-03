106ccc79806d323b44dd1dce122049b8296af245
brankoEU <30345282+brankoEU@users.noreply.github.com>
2017-08-03 10:04:42 +0300

    commit 6
    modificat cod pt numere compuse

--- JuniorMind02/UnitTest1/UnitTest1/RomanNumbersTests.cpp
+++ JuniorMind02/UnitTest1/UnitTest1/RomanNumbersTests.cpp
@@ -36,12 +36,24 @@ namespace RomanNumbers
 			Assert::AreEqual("LXX", ConvertToRoman(70).c_str());
 		}
 
+		TEST_METHOD(ConvertToRomanTestFor74)
+		{
+			Assert::AreEqual("LXXIV", ConvertToRoman(74).c_str());
+		}
+
+		TEST_METHOD(ConvertToRomanTestFor48)
+		{
+			Assert::AreEqual("XLVIII", ConvertToRoman(48).c_str());
+		}
+
 		string ConvertToRoman(int number)
 		{
+			string romanNr;
 			const string smallRomanNumbers[] = { "I" , "II", "III", "IV", "V", "VI", "VII", "VIII", "IX" };
 			const string bigRomanNumbers[] = { "X" , "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC" , "C" };
-			if (number / 10 > 0) return bigRomanNumbers[number / 10 - 1];
-			if (number % 10 > 0) return smallRomanNumbers[number - 1];
+			if (number / 10 > 0) romanNr += bigRomanNumbers[number / 10 - 1];
+			if (number % 10 > 0) romanNr += smallRomanNumbers[number % 10 - 1];
+			return romanNr;
 		}
 
 	};
