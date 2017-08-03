9bc4e3d6523203bbceeea987ec82adf9cefc5074
brankoEU <30345282+brankoEU@users.noreply.github.com>
2017-08-03 15:56:28 +0300

    commit 2

--- JuniorMind03/LotoChances/LotoChances/LotoChancesTests.cpp
+++ JuniorMind03/LotoChances/LotoChances/LotoChancesTests.cpp
@@ -1,6 +1,7 @@
 #include "stdafx.h"
 #include "CppUnitTest.h"
 
+
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace LotoChances
@@ -9,14 +10,36 @@ namespace LotoChances
 	{
 	public:
 		
+		TEST_METHOD(CalculateFactorialTest)
+		{
+			Assert::AreEqual(6.0, CalculateFactorial(3));
+		}
+
 		TEST_METHOD(ChanceOfOneFromTenTest)
 		{
-			Assert::AreEqual(10, CalculateLotoChance(1, 10));
+			Assert::AreEqual(10.0, CalculateLotoChance(1, 10));
+		}
+
+		TEST_METHOD(ChanceOf3From10Test)
+		{
+			Assert::AreEqual(120.0, CalculateLotoChance(3, 10));
 		}
 
 		double CalculateLotoChance(int numbers, int loto)
 		{
-			return 0;
+			double chance = CalculateFactorial(loto) / (CalculateFactorial(numbers)*CalculateFactorial(loto - numbers));
+			return chance;
+		}
+
+		double CalculateFactorial(int n)
+		{
+			int fact = 1;
+			for (int i = 1; i <= n; i++)
+			{
+				fact *= i;
+			}
+
+			return fact;
 		}
 
 	};
