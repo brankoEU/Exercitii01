34995b06ea8dff99a377800c47ec6edb99bdece0
brankoEU <30345282+brankoEU@users.noreply.github.com>
2017-08-03 09:44:30 +0300

    commit 4
    modificat sirul roman, pt orice nr pana la 9

--- JuniorMind02/UnitTest1/UnitTest1/RomanNumbersTests.cpp
+++ JuniorMind02/UnitTest1/UnitTest1/RomanNumbersTests.cpp
@@ -11,30 +11,26 @@ namespace RomanNumbers
 	{
 	public:
 		
-		TEST_METHOD(ConvertToRomanTest1)
+		TEST_METHOD(ConvertToRomanTestFor1)
 		{
 			Assert::AreEqual("I", ConvertToRoman(1).c_str());
 		}
 
-		TEST_METHOD(ConvertToRomanTest2)
+		TEST_METHOD(ConvertToRomanTestFor2)
 		{
 			Assert::AreEqual("II", ConvertToRoman(2).c_str());
 		}
 
-		TEST_METHOD(ConvertToRomanTest3)
+		TEST_METHOD(ConvertToRomanTestFor7)
 		{
-			Assert::AreEqual("III", ConvertToRoman(3).c_str());
+			Assert::AreEqual("VII", ConvertToRoman(7).c_str());
 		}
 
 		string ConvertToRoman(int number)
 		{
-			string romanNr;
-			const string romanNumbers[] = { "I" , "V" };
-			if (number % 10 > 2) romanNr = romanNumbers[0] + romanNumbers[0] + romanNumbers[0];
-			else if (number % 10 > 1) romanNr = romanNumbers[0] + romanNumbers[0];
-			else romanNr = romanNumbers[0];
-
-			return romanNr;
+			const string smallRomanNumbers[] = { "I" , "II", "III", "IV", "V", "VI", "VII", "VIII", "IX" };
+			
+			if (number % 10 > 0) return smallRomanNumbers[number - 1];
 		}
 
 	};
