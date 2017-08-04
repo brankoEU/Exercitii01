730104f40afdc318e7a4131e3174d89476e7d674
brankoEU <30345282+brankoEU@users.noreply.github.com>
2017-08-04 22:46:10 +0300

    prefix commit3

--- JuniorMind03/Prefix/Prefix/PrefixTests.cpp
+++ JuniorMind03/Prefix/Prefix/PrefixTests.cpp
@@ -16,14 +16,29 @@ namespace Prefix
 			Assert::AreEqual("a", GetTextPrefix("a", "a").c_str());
 		}
 
+		TEST_METHOD(TestMethod2)
+		{
+			Assert::AreEqual("aa", GetTextPrefix("aac", "aab").c_str());
+		}
+
 		string GetTextPrefix(string text1, string text2)
 		{
 			string prefix = "";
-			if (text1[0]==text2[0])
+			bool found = true;
+			int index = 0;
+			while (found && (StringNotEnded(index, text1, text2)))
 			{
-				prefix += text1[0];
+				if (text1[index] == text2[index]) prefix += text1[0];
+				index++;
 			}
+			
+			
 			return prefix;
+		}
+
+		bool StringNotEnded(int i, std::string &text1, std::string &text2)
+		{
+			return i<text1.length() || i<text2.length();
 		}
 
 	};
