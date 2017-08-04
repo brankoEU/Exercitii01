9f6a27e695bcf67fc702881b48a1cfb911d6d42c
brankoEU <30345282+brankoEU@users.noreply.github.com>
2017-08-04 19:30:52 +0300

    commit 4

--- JuniorMind03/ExcelColumns/ExcelColumns/ExcelColumnsTests.cpp
+++ JuniorMind03/ExcelColumns/ExcelColumns/ExcelColumnsTests.cpp
@@ -1,7 +1,6 @@
 #include "stdafx.h"
 #include "CppUnitTest.h"
 #include <string>
-#include <iostream>
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 using namespace std;
@@ -22,15 +21,23 @@ namespace ExcelColumns
 			Assert::AreEqual("AB", LetterCombinationForExcelColums(28).c_str());
 		}
 
+		TEST_METHOD(ColumnWithTreeLeters)
+		{
+			Assert::AreEqual("ACZ", LetterCombinationForExcelColums(754).c_str());
+		}
+
 		string LetterCombinationForExcelColums(int number)
 		{
 			const string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 			string columnName = "";
+			int strLength = alphabet.length();
 
-			while (number % 26 > 0)
+			while (number > 0)
 			{
-				columnName = alphabet[number % 26 - 1] + columnName;
-				number = number / 26;
+				if (number % strLength == 0) { columnName = alphabet[strLength - 1] + columnName; }
+				else
+				{ columnName = alphabet[number % strLength - 1] + columnName; }
+				number /= strLength;
 			}
 			
 			return columnName;
