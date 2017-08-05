02bc88003ed27658adc658e9665770541288e4e4
brankoEU <30345282+brankoEU@users.noreply.github.com>
2017-08-05 10:58:31 +0300

    commit 3
    extract function CalculateCMMDC and add formula for CMMMC

--- JuniorMind03/LunchMeeting/LunchMeeting/LunchMeetingTests.cpp
+++ JuniorMind03/LunchMeeting/LunchMeeting/LunchMeetingTests.cpp
@@ -11,20 +11,36 @@ namespace LunchMeeting
 		
 		TEST_METHOD(TestCMMDC2And4)
 		{
-			Assert::AreEqual(2, CalculateDaysToNextMeeting(2, 4));
+			Assert::AreEqual(2, CalculateCMMDC(2, 4));
+		}
+
+		TEST_METHOD(TestCMMDC9And27)
+		{
+			Assert::AreEqual(9, CalculateCMMDC(9, 27));
+		}
+
+		TEST_METHOD(TestCMMC4And6)
+		{
+			Assert::AreEqual(12, CalculateDaysToNextMeeting(4, 6));
 		}
 
 		int CalculateDaysToNextMeeting(int numberOne, int numberTwo)
 		{
-			//calcul CMMDC
-			while (numberTwo != 0)
-			{
-				int aux = numberTwo;
-			    numberTwo = numberOne%numberTwo;
-				numberOne = aux;
-			}
+			//calcul CMMMC
+			
+			return numberOne*numberTwo / CalculateCMMDC(numberTwo, numberOne);
+		}
 
-			return numberOne;
+		int CalculateCMMDC(int a, int b)
+		{
+			//calcul CMMDC
+			while (b != 0)
+			{
+				int aux = b;
+				b = a % b;
+				a = aux;
+			}
+			return a;
 		}
 
 	};
