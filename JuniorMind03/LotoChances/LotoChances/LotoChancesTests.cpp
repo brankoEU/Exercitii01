b44c06ed087684a0d31c02bfb29e4de1518fd3a7
brankoEU <30345282+brankoEU@users.noreply.github.com>
2017-08-21 23:43:14 +0300

    lotto update

--- JuniorMind03/LotoChances/LotoChances/LotoChancesTests.cpp
+++ JuniorMind03/LotoChances/LotoChances/LotoChancesTests.cpp
@@ -12,38 +12,43 @@ namespace LotoChances
 		
 		TEST_METHOD(ChanceOf1From10Test)
 		{
-			Assert::AreEqual(0.1, CalculateLotoChance(1, 10));
+			Assert::AreEqual(0.1, CalculateLotoChance(1, 1, 10));
 		}
 
 		TEST_METHOD(ChanceOf3From10Test)
 		{
-			Assert::AreEqual(0.00833333, CalculateLotoChance(3, 10),0.00000001);
+			Assert::AreEqual(0.00833333, CalculateLotoChance(3, 3, 10),0.00000001);
 		}
 
 		TEST_METHOD(ChanceOf6From49Test)
 		{
-			Assert::AreEqual(0.00000007151123842018516, CalculateLotoChance(6, 49));
+			Assert::AreEqual(0.00000007151123842018516, CalculateLotoChance(6, 6, 49));
 		}
 
 		TEST_METHOD(ChanceOf5From40Test)
 		{
-			Assert::AreEqual(0.00000151973, CalculateLotoChance(5, 40),0.00000001);
+			Assert::AreEqual(0.00000151973, CalculateLotoChance(5, 5, 40),0.00000001);
 		}
 
 		TEST_METHOD(ChanceOf5From49Test)
 		{
-			Assert::AreEqual(5.24415748e-7, CalculateLotoChance(5, 49), 0.00000001);
+			Assert::AreEqual(0.00001844989, CalculateLotoChance(5, 6, 49), 0.00000001);
 		}
 
 		TEST_METHOD(ChanceOf4From49Test)
 		{
-			Assert::AreEqual(0.00000471974, CalculateLotoChance(4, 49), 0.00000001);
+			Assert::AreEqual(0.00096861681, CalculateLotoChance(4, 6, 49), 0.00000001);
 		}
 
-		double CalculateLotoChance(int numbers, int loto)
+		double CalculateLotoChance(int winNumbers, int extractedNumbers, int totalNumbers)
 		{
 			
-			return 1 / CalculateCombinations(loto, numbers);
+			return 1 / CalculateProbability(winNumbers ,totalNumbers, extractedNumbers);
+		}
+
+		double CalculateProbability(int m, int n, int k)
+		{
+			return CalculateCombinations(n, k) / CalculateCombinations(k, m)*CalculateCombinations(n - k, k - m);
 		}
 
 		double CalculateCombinations(int n, int k)
