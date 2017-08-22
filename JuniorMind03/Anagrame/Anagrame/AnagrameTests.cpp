ec6fc76dad662c26f07765020e3f2bea13496a98
brankoEU <30345282+brankoEU@users.noreply.github.com>
2017-08-22 12:33:48 +0300

    commit 4

--- JuniorMind03/Anagrame/Anagrame/AnagrameTests.cpp
+++ JuniorMind03/Anagrame/Anagrame/AnagrameTests.cpp
@@ -1,6 +1,7 @@
 #include "stdafx.h"
 #include "CppUnitTest.h"
 #include <string>
+#include <algorithm>
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 using namespace std;
@@ -11,22 +12,46 @@ namespace Anagrame
 	{
 	public:
 		
+		TEST_METHOD(TestDuplicates)
+		{
+			Assert::AreEqual(2, CheckDuplicates("aac", 'a'));
+		}
+
 		TEST_METHOD(TestMethod1)
 		{
-			Assert::AreEqual(2.0, CalculateAnagrams("ab"));
+			Assert::AreEqual(2, CalculateAnagrams("ab"));
 		}
 
 		TEST_METHOD(TestMethod2)
 		{
-			Assert::AreEqual(6.0, CalculateAnagrams("abc"));
+			Assert::AreEqual(6, CalculateAnagrams("abc"));
 		}
 
-		double CalculateAnagrams(string text)
+		TEST_METHOD(TestMethod3)
 		{
-			return CalculateFactorial(text.length())/CalculateFactorial(text.length()-2);
+			Assert::AreEqual(3, CalculateAnagrams("aac"));
 		}
 
-		double CalculateFactorial(int n)
+
+
+		int CalculateAnagrams(string text)
+		{
+			int repetitions = 0;
+			for (int i = 0; i < text.length(); i++)
+			{
+				if(CheckDuplicates(text, text[i])>1)
+				repetitions += CheckDuplicates(text, text[i]);
+			}
+			return repetitions;
+			//return CalculateFactorial(text.length())/CalculateFactorial(repetitions);
+		}
+
+		int CheckDuplicates(string text, char charToCheck)
+		{
+			return count(begin(text), end(text), charToCheck);
+		}
+
+		int CalculateFactorial(int n)
 		{
 			double fact = 1;
 			for (int i = 1; i <= n; i++)
