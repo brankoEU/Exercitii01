8816faf290183781a0b5f24077c4380dae09eed6
brankoEU <branko.tudorovici@gmail.com>
2017-09-26 15:55:56 +0300

    Initial Commit Alarm

---
+++ JuniorMind05/ClockAlarm/ClockAlarm/ClockAlarmTests.cpp
@@ -0,0 +1,29 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace ClockAlarm
+{		
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			//Assert::IsTrue(Alarm());
+		}
+
+		enum Days
+		{
+			Monday,
+			Tuesday,
+			Wednesday,
+			Thursday,
+			Friday,
+			Saturday,
+			Sunday
+		};
+
+	};
+}
