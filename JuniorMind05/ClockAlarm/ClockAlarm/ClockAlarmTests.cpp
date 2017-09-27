14fc8f5bfe89a5edcd35bf4ebd1cd6558e041aa4
brankoEU <branko.tudorovici@gmail.com>
2017-09-27 11:16:27 +0300

    error assert

--- JuniorMind05/ClockAlarm/ClockAlarm/ClockAlarmTests.cpp
+++ JuniorMind05/ClockAlarm/ClockAlarm/ClockAlarmTests.cpp
@@ -5,16 +5,16 @@ using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace ClockAlarm
 {		
-	TEST_CLASS(UnitTest1)
+	TEST_CLASS(ClockAlarmTests)
 	{
 	public:
 		
 		TEST_METHOD(TestMethod1)
 		{
-			//Assert::IsTrue(Alarm());
+			Assert::AreEqual(true, AlarmRinger(6, Monday, { Monday | 6 }));
 		}
-
-		enum Days
+		
+		enum WeekDays
 		{
 			Monday,
 			Tuesday,
@@ -24,6 +24,22 @@ namespace ClockAlarm
 			Saturday,
 			Sunday
 		};
+		
+		struct Alarm
+		{
+			WeekDays day;
+			int hour;
+			Alarm(WeekDays day, int hour)
+			{
+				this->day = day;
+				this->hour = hour;
+			}
+		};
+
+		bool AlarmRinger(int hour, WeekDays day, Alarm alarm)
+		{
+			return false;
+		}
 
 	};
 }
