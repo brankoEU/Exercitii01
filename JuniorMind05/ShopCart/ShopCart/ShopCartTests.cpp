153baa1c71bcd5cd9d30062f062122678879938a
brankoEU <branko.tudorovici@gmail.com>
2017-10-03 16:24:17 +0300

    Cheapest Product and initialisation list added

--- JuniorMind05/ShopCart/ShopCart/ShopCartTests.cpp
+++ JuniorMind05/ShopCart/ShopCart/ShopCartTests.cpp
@@ -16,6 +16,11 @@ namespace ShopCart
 			Assert::AreEqual(10.8, TotalShopingCart());
 		}
 
+		TEST_METHOD(CheapestProduct)
+		{
+			Assert::AreEqual("Water", FindCheapestProduct().c_str());
+		}
+
 		struct Product
 		{
 			string name;
@@ -27,24 +32,26 @@ namespace ShopCart
 			}
 		};
 
-		vector<Product> ShoppingCart;
-
-		void PopulateShop()
-		{
-			ShoppingCart.emplace_back("Water", 2.5);
-			ShoppingCart.emplace_back("Bread", 3.1);
-			ShoppingCart.emplace_back("Orange", 5.2);
-		}
+		vector<Product> ShoppingCart = { { "Water", 2.5 } ,{ "Bread", 3.1 } ,{ "Orange", 5.2 } };
 
 		double TotalShopingCart()
 		{
-			PopulateShop();
 			double totalPriceSC = 0;
 			for (int i = 0; i < ShoppingCart.size(); i++)
 			{
 				totalPriceSC += ShoppingCart[i].price;
 			}
 			return totalPriceSC;
+		} 
+
+		string FindCheapestProduct()
+		{
+			string cheapest = ShoppingCart[0].name;
+			for (int i = 1; i < ShoppingCart.size(); i++)
+			{
+				if (ShoppingCart[i].price < ShoppingCart[i - 1].price) cheapest = ShoppingCart[i].name;
+			}
+			return cheapest;
 		}
 	};
 }
