96f3fecff925ace8cb24793f6868b0ff52df9201
brankoEU <branko.tudorovici@gmail.com>
2017-10-03 12:26:56 +0300

    commit 1

--- JuniorMind05/ShopCart/ShopCart/ShopCartTests.cpp
+++ JuniorMind05/ShopCart/ShopCart/ShopCartTests.cpp
@@ -1,18 +1,47 @@
 #include "stdafx.h"
 #include "CppUnitTest.h"
+#include <vector>
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace std;
 
 namespace ShopCart
 {		
-	TEST_CLASS(UnitTest1)
+	TEST_CLASS(ShoppingCartTests)
 	{
 	public:
 		
-		TEST_METHOD(TestMethod1)
+		TEST_METHOD(TotalPriceSHOPPING)
 		{
-			// TODO: Your test code here
+			Assert::AreEqual(10.8, TotalShopingCart());
 		}
 
+		struct Product
+		{
+			string name;
+			double price;
+			Product(string name, double price)
+			{
+				this->name = name;
+				this->price = price;
+			}
+		};
+
+		Product ShoppingCart[] =
+		{
+			Product(name = "Water", price = 2.5),
+			Product(name = "Bred", price = 3.1),
+			Product(name = "Orange", price = 5.2)
+		};
+
+		double TotalShopingCart()
+		{
+			double totalPriceSC = 0;
+			for (int i = 0; i < sizeof(ShoppingCart[0]); i++)
+			{
+				totalPriceSC += ShoppingCart[i].price;
+			}
+			return totalPriceSC;
+		}
 	};
 }
