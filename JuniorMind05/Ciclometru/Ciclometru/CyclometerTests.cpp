7bbb7594e8a842df8e1e27a482f54c6b6e63d97e
brankoEU <branko.tudorovici@gmail.com>
2017-10-04 14:50:47 +0300

    best average speed per cyclist

--- JuniorMind05/Ciclometru/Ciclometru/CyclometerTests.cpp
+++ JuniorMind05/Ciclometru/Ciclometru/CyclometerTests.cpp
@@ -17,9 +17,26 @@ namespace Ciclometru
 			Assert::AreEqual(2562.24, CalculateTotalDistance(data_base), 0.000000000001);
 		}
 
+		TEST_METHOD(MaximumSpeed)
+		{
+			vector<Cyclometer> data_base = { { "Ionut",26,{ 2,2,3,1,3 } } ,{ "Alex",20,{ 3,1,3,4,2 } } ,{ "Marius",30,{ 1,2,1,4,1 } } };
+			Assert::AreEqual("Marius", FindMaximumSpeed(data_base).c_str());
+		}
+
 		TEST_METHOD(TestDistancePerCyclist)
 		{
 			Assert::AreEqual(898.04, DistancePerCyclist(26, { 2,2,3,1,3 }));
+		}
+
+		TEST_METHOD(TestSpeedPerSecond)
+		{
+			Assert::AreEqual(163.28, SpeedPerSecond(26, 2));
+		}
+
+		TEST_METHOD(BestAverageSpeed)
+		{
+			vector<Cyclometer> data_base = { { "Ionut",26,{ 2,2,3,1,3 } } ,{ "Alex",20,{ 3,1,3,4,2 } } ,{ "Marius",30,{ 1,2,1,4,1 } } };
+			Assert::AreEqual("Ionut", BestAverageSpeedPerCyclist(data_base).c_str());
 		}
 
 		struct Cyclometer
@@ -38,6 +55,11 @@ namespace Ciclometru
 		double Circumference(double diameter)
 		{
 			return diameter * 3.14;
+		}
+
+		double SpeedPerSecond(double diameter, double rotations)
+		{
+			return Circumference(diameter)*rotations;
 		}
 
 		double DistancePerCyclist(double diameter, vector<double> rotations)
@@ -60,5 +82,24 @@ namespace Ciclometru
 			return totalDistance;
 		}
 
+		string FindMaximumSpeed(vector<Cyclometer> data)
+		{
+			return "";
+		}
+
+		string BestAverageSpeedPerCyclist(vector<Cyclometer> data)
+		{
+			double speed = 0;
+			string bestAverage;
+			for (int i = 0; i < data.size(); i++)
+			{
+				if ((DistancePerCyclist(data[i].diameter, data[i].records) / data[i].records.size()) > speed)
+				{
+					speed = DistancePerCyclist(data[i].diameter, data[i].records) / data[i].records.size();
+					bestAverage = data[i].name;
+				}
+			}
+			return bestAverage;
+		}
 	};
 }
