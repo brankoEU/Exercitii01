865bc0bdd9acd5fb4dc714e6f065e0086823cae7
brankoEU <branko.tudorovici@gmail.com>
2017-10-05 11:49:45 +0300

    Cyclometer Final Working

--- JuniorMind05/Ciclometru/Ciclometru/CyclometerTests.cpp
+++ JuniorMind05/Ciclometru/Ciclometru/CyclometerTests.cpp
@@ -17,20 +17,18 @@ namespace Ciclometru
 			Assert::AreEqual(2562.24, CalculateTotalDistance(data_base), 0.000000000001);
 		}
 
-		TEST_METHOD(TestDistancePerCyclist)
-		{
-			Assert::AreEqual(898.04, DistancePerCyclist(26, { 2,2,3,1,3 }));
-		}
-
-		TEST_METHOD(TestSpeedPerSecond)
-		{
-			Assert::AreEqual(163.28, SpeedPerSecond(26, 2));
-		}
-
 		TEST_METHOD(BestAverageSpeed)
 		{
 			vector<Cyclometer> data_base = { { "Ionut",26,{ 2,2,3,1,3 } } ,{ "Alex",20,{ 3,1,3,4,2 } } ,{ "Marius",30,{ 1,2,1,4,1 } } };
 			Assert::AreEqual("Ionut", BestAverageSpeedPerCyclist(data_base).c_str());
+		}
+
+		TEST_METHOD(CyclistBestSpeed)
+		{
+			vector<Cyclometer> data_base = { { "Ionut",26,{ 2,2,3,1,3 } } ,{ "Alex",20,{ 3,1,3,4,2 } } ,{ "Marius",30,{ 1,2,1,4,1 } } };
+			Result result = CyclistBestSpeed(data_base);
+			Assert::AreEqual("Marius", result.name.c_str());
+			Assert::AreEqual(4, result.second);
 		}
 
 		struct Cyclometer
@@ -96,5 +94,38 @@ namespace Ciclometru
 			}
 			return bestAverage;
 		}
+
+		int GetMaxRotationPos(vector<double> rotations)
+		{
+			double maxRotations = rotations[0];
+			int position;
+			for (int i = 1; i < rotations.size(); i++)
+			{
+				if (rotations[i] > maxRotations) 
+				{
+					maxRotations = rotations[i];
+					position = i+1;
+				}
+			}
+			return position;
+		}
+
+		Result CyclistBestSpeed(vector<Cyclometer> data)
+		{
+			double bestSpeed = 0;
+		    Result result;
+			for (int i = 0; i < data.size(); i++)
+			{
+				if (bestSpeed < SpeedPerSecond(data[i].diameter, data[i].records[GetMaxRotationPos(data[i].records) - 1]))
+				{
+					bestSpeed = SpeedPerSecond(data[i].diameter, data[i].records[GetMaxRotationPos(data[i].records) - 1]);
+					result.name = data[i].name;
+					result.second = GetMaxRotationPos(data[i].records);
+				}
+			}
+			
+			return result;
+		}
+
 	};
 }
