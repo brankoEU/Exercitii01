23c44bd3e764d6e2f0d2726eefb9ee7228f80e3f
brankoEU <branko.tudorovici@gmail.com>
2017-10-10 16:59:06 +0300

    Generate same password

--- JuniorMind05/Password/Password/PasswordTests.cpp
+++ JuniorMind05/Password/Password/PasswordTests.cpp
@@ -1,17 +1,41 @@
 #include "stdafx.h"
 #include "CppUnitTest.h"
+#include <random>
+#include <string>
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace std;
 
 namespace Password
 {		
-	TEST_CLASS(UnitTest1)
+	TEST_CLASS(PasswordTest)
 	{
 	public:
 		
 		TEST_METHOD(TestMethod1)
 		{
-			// TODO: Your test code here
+			Assert::AreEqual("", GeneratePassword(5).c_str());
+		}
+
+		string GeneratePassword(int passlenght)
+		{
+			string ambiguous = "{}[]()/\\'\"`~,;:.<>";
+			string symbols = "!@#$%^&*-_+=|?";
+			string numbers = "1234567890";
+			string lowercase = "abcdefghijklmnopqrstuvwxyz";
+			string uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+			string similar = "iIl10oO";
+			string password = "";
+
+			default_random_engine generator;
+			uniform_int_distribution<int> asciiChar(33, 126);
+
+			while (password.size() < passlenght)
+			{
+				password += char(asciiChar(generator));
+			}
+			
+			return password;
 		}
 
 	};
