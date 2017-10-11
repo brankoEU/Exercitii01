64ce134c2a06530a174640b2982ab6fd5ad5783f
brankoEU <branko.tudorovici@gmail.com>
2017-10-11 16:45:47 +0300

    Options Added

--- JuniorMind05/Password/Password/PasswordTests.cpp
+++ JuniorMind05/Password/Password/PasswordTests.cpp
@@ -15,35 +15,51 @@ namespace Password
 		
 		TEST_METHOD(TestMethod1)
 		{
-			Assert::AreEqual("", GeneratePassword(7).c_str());
+			Assert::AreEqual("", GeneratePassword({ 7,2,2,1,true,true }).c_str());
 		}
 
-		
+		struct PasswordOptions
+		{
+			int passLenght;
+			int uppercase;
+			int numbers;
+			int symbols;
+			bool excludeAmbiguius;
+			bool excludeSimilar;
+		};
 
-		string GeneratePassword(int passlenght)
+		string GeneratePassword(PasswordOptions options)
 		{
 			string ambiguous = "{}[]()/\\'\"`~,;:.<>";
 			string symbols = "!@#$%^&*-_+=|?";
 			string numbers = "1234567890";
+			string lowercase = "abcdefghijklmnopqrstuvwxyz";
 			string uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 			string similar = "iIl10oO";
 			string password = "";
-			int countUpper = 0, countSymbols = 0, countNumbers = 0;
+			int countUpper = 0, countSymbols = 0, countNumbers = 0, countLower = 0;
 
 			static random_device rd;
 			static mt19937 generator(rd());
 			uniform_int_distribution<int> asciiChar(33, 126);
 
-			while (password.size() < passlenght)
+			while (password.size() < options.passLenght)
 			{
 				char nextChar = char(asciiChar(generator));
-				if (!CheckChar(ambiguous, nextChar) && !CheckChar(similar, nextChar)) { password += nextChar; }
-				if (CheckChar(symbols, nextChar)) { countSymbols++; }
-				if (CheckChar(uppercase, nextChar)) { countUpper++; }
-				if (CheckChar(numbers, nextChar)) { countNumbers++; }
+				if (CheckChar(ambiguous, nextChar) && !options.excludeAmbiguius) { password += nextChar; }
+				if (CheckChar(similar, nextChar) && !options.excludeSimilar) { password += nextChar; }
+				if (CheckChar(symbols, nextChar) && countSymbols < options.symbols) { countSymbols++; password += nextChar; }
+				if (CheckChar(lowercase, nextChar) && countLower < (GetLowerCaseLenght(options))) { countLower++; password += nextChar; }
+				if (CheckChar(uppercase, nextChar) && countUpper < options.uppercase) { countUpper++; password += nextChar; }
+				if (CheckChar(numbers, nextChar) && countNumbers < options.numbers) { countNumbers++; password += nextChar; }
 			}
 
 			return password;
+		}
+
+		int GetLowerCaseLenght(PasswordOptions options)
+		{
+			return options.passLenght - options.numbers - options.symbols - options.uppercase;
 		}
 
 		bool CheckChar(string str, char toCheck)
