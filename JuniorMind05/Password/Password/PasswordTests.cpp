ed8bb73f06027da45cd1e20eafb7ff2ac429c66f
brankoEU <branko.tudorovici@gmail.com>
2017-10-13 16:22:48 +0300

    Extract Header password

--- JuniorMind05/Password/Password/PasswordTests.cpp
+++ JuniorMind05/Password/Password/PasswordTests.cpp
@@ -1,8 +1,6 @@
 #include "stdafx.h"
 #include "CppUnitTest.h"
-#include <random>
-#include <algorithm>
-#include <string>
+#include "password.h"
 
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
@@ -10,18 +8,6 @@ using namespace std;
 
 namespace Password
 {	
-	class Generator {
-		std::uniform_int_distribution<int> dis;
-		std::mt19937 gen;
-	public:
-		Generator(int lower = 33, int upper = 126)
-			: gen(std::random_device()()), dis(lower, upper) {}
-
-		int operator()() {
-			return dis(gen);
-		}
-	};
-
 	TEST_CLASS(PasswordTest)
 	{
 	public:
@@ -65,116 +51,6 @@ namespace Password
 			Assert::AreEqual(true, VerifyAmbiguousAndSimilars(password, options.strSets.similar));
 		}
 		
-		struct PasswordOptions
-		{
-			int passLenght;
-			int uppercase;
-			int numbers;
-			int symbols;
-			bool excludeAmbiguius;
-			bool excludeSimilar;
-
-			struct StringSets
-			{
-				string ambiguous = "{}[]()/\\'`~,;:.<>\"";
-				string symbols = "!@#$%^&*-_+=|?";
-				string numbers = "1234567890";
-				string lowercase = "abcdefghijklmnopqrstuvwxyz";
-				string uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-				string similar = "iIl10oO";
-			}strSets;
-
-			string GetAllowed()
-			{
-				string allowed = strSets.lowercase;
-				if (!excludeAmbiguius) allowed += strSets.ambiguous;
-				if (!excludeSimilar) allowed += strSets.similar;
-				return allowed;
-			}
-
-			string GetRestricted()
-			{
-				string restricted = "";
-				if (excludeAmbiguius) restricted += strSets.ambiguous;
-				if (excludeSimilar) restricted += strSets.similar;
-				return restricted;
-			}
-
-			bool CheckChar(string str, char toCheck)
-			{
-				for each(char c in str)
-				{
-					if (toCheck == c) return true;
-				}
-				return false;
-			}
-
-			string GetUpperCases()
-			{
-				Generator rndNo;
-				string str = "";
-				int i = 0;
-				while (i < uppercase)
-				{
-					char nextChar = char(rndNo());
-					if (CheckChar(strSets.uppercase, nextChar) && !CheckChar(GetRestricted(), nextChar))
-					{
-						str += nextChar;
-						i++;
-					}
-				}
-				return str;
-			}
-
-			string GetNumbers()
-			{
-				Generator rndNo;
-				string str = "";
-				int i = 0;
-				while (i < numbers)
-				{
-					char nextChar = char(rndNo());
-					if (CheckChar(strSets.numbers, nextChar) && !CheckChar(GetRestricted(), nextChar))
-					{
-						str += nextChar;
-						i++;
-					}
-				}
-				return str;
-			}
-
-			string GetSymbols()
-			{
-				Generator rndNo;
-				string str = "";
-				int i = 0;
-				while (i < symbols)
-				{
-					char nextChar = char(rndNo());
-					if (CheckChar(strSets.symbols, nextChar) && !CheckChar(GetRestricted(), nextChar))
-					{
-						str += nextChar;
-						i++;
-					}
-				}
-				return str;
-			}
-
-			string GeneratePassword()
-			{
-				Generator rndNo;
-				string password = GetUpperCases() + GetNumbers() + GetSymbols();
-
-				while (password.size() < passLenght)
-				{
-					char nextChar = char(rndNo());
-					if (CheckChar(GetAllowed(), nextChar) && !CheckChar(GetRestricted(), nextChar)) { password += nextChar; }
-				}
-				return password;
-			}
-
-		};
-
 		int VerifyPassword(string password, string str)
 		{
 			int count = 0;
---
+++ JuniorMind05/Password/Password/password.h
@@ -0,0 +1,129 @@
+#ifndef PASSWORD_H
+#define PASSWORD_H
+
+#include <string>
+#include <random>
+
+class Generator {
+	std::uniform_int_distribution<int> dis;
+	std::mt19937 gen;
+public:
+	Generator(int lower = 33, int upper = 126)
+		: gen(std::random_device()()), dis(lower, upper) {}
+
+	int operator()() {
+		return dis(gen);
+	}
+};
+
+struct PasswordOptions
+{
+	int passLenght;
+	int uppercase;
+	int numbers;
+	int symbols;
+	bool excludeAmbiguius;
+	bool excludeSimilar;
+
+	struct StringSets
+	{
+		std::string ambiguous = "{}[]()/\\'`~,;:.<>\"";
+		std::string symbols = "!@#$%^&*-_+=|?";
+		std::string numbers = "1234567890";
+		std::string lowercase = "abcdefghijklmnopqrstuvwxyz";
+		std::string uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		std::string similar = "iIl10oO";
+	}strSets;
+
+	std::string GetAllowed()
+	{
+		std::string allowed = strSets.lowercase;
+		if (!excludeAmbiguius) allowed += strSets.ambiguous;
+		if (!excludeSimilar) allowed += strSets.similar;
+		return allowed;
+	}
+
+	std::string GetRestricted()
+	{
+		std::string restricted = "";
+		if (excludeAmbiguius) restricted += strSets.ambiguous;
+		if (excludeSimilar) restricted += strSets.similar;
+		return restricted;
+	}
+
+	bool CheckChar(std::string str, char toCheck)
+	{
+		for each(char c in str)
+		{
+			if (toCheck == c) return true;
+		}
+		return false;
+	}
+
+	std::string GetUpperCases()
+	{
+		Generator rndNo;
+		std::string str = "";
+		int i = 0;
+		while (i < uppercase)
+		{
+			char nextChar = char(rndNo());
+			if (CheckChar(strSets.uppercase, nextChar) && !CheckChar(GetRestricted(), nextChar))
+			{
+				str += nextChar;
+				i++;
+			}
+		}
+		return str;
+	}
+
+	std::string GetNumbers()
+	{
+		Generator rndNo;
+		std::string str = "";
+		int i = 0;
+		while (i < numbers)
+		{
+			char nextChar = char(rndNo());
+			if (CheckChar(strSets.numbers, nextChar) && !CheckChar(GetRestricted(), nextChar))
+			{
+				str += nextChar;
+				i++;
+			}
+		}
+		return str;
+	}
+
+	std::string GetSymbols()
+	{
+		Generator rndNo;
+		std::string str = "";
+		int i = 0;
+		while (i < symbols)
+		{
+			char nextChar = char(rndNo());
+			if (CheckChar(strSets.symbols, nextChar) && !CheckChar(GetRestricted(), nextChar))
+			{
+				str += nextChar;
+				i++;
+			}
+		}
+		return str;
+	}
+
+	std::string GeneratePassword()
+	{
+		Generator rndNo;
+		std::string password = GetUpperCases() + GetNumbers() + GetSymbols();
+
+		while (password.size() < passLenght)
+		{
+			char nextChar = char(rndNo());
+			if (CheckChar(GetAllowed(), nextChar) && !CheckChar(GetRestricted(), nextChar)) { password += nextChar; }
+		}
+		return password;
+	}
+
+};
+
+#endif
