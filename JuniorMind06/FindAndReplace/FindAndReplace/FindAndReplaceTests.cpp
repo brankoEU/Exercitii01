98506f7903992faac23035d2d6ab25e2d451e05b
brankoEU <30345282+brankoEU@users.noreply.github.com>
2017-10-17 20:54:13 +0300

    test 3 red

--- JuniorMind06/FindAndReplace/FindAndReplace/FindAndReplaceTests.cpp
+++ JuniorMind06/FindAndReplace/FindAndReplace/FindAndReplaceTests.cpp
@@ -15,10 +15,20 @@ namespace FindAndReplace
 			Assert::AreEqual("ama", FindAndReplace("ada",'d',"m").c_str());
 		}
 
+		TEST_METHOD(TestMethod2)
+		{
+			Assert::AreEqual("amnamn", FindAndReplace("adad", 'd', "mn").c_str());
+		}
+
+		TEST_METHOD(TestMethod3)
+		{
+			Assert::AreEqual("telefon", FindAndReplace("txlxfon", 'x', "e").c_str());
+		}
+
 		string FindAndReplace(string str, char c, string replacer)
 		{
 			if (str.find(c) == string::npos) return str;
-			return str.replace(str.find(c), str.find(c),replacer);
+			return FindAndReplace((str.replace(str.find(c), str.find(c),replacer)),c,replacer);
 		}
 
 	};
