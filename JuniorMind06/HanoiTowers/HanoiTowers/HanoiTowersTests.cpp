03949d4b6bca35a097bc03d2e88a7b1ad865300b
brankoEU <branko.tudorovici@gmail.com>
2017-10-19 14:09:44 +0300

    HanoiTowers New Proj

---
+++ JuniorMind06/HanoiTowers/HanoiTowers/HanoiTowersTests.cpp
@@ -0,0 +1,18 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace HanoiTowers
+{		
+	TEST_CLASS(HanoiTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			// TODO: Your test code here
+		}
+
+	};
+}
