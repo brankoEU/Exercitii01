20cf60c429a1356f7cd20437c7138f570ee7a584
brankoEU <branko.tudorovici@gmail.com>
2017-10-19 12:35:15 +0300

    One more test

--- JuniorMind06/PascalTriangle/PascalTriangle/PascalTriangleTests.cpp
+++ JuniorMind06/PascalTriangle/PascalTriangle/PascalTriangleTests.cpp
@@ -35,9 +35,14 @@ namespace PascalTriangle
 			Assert::AreEqual({ 1,5,10,10,5,1 }, GenerateRow(5));
 		}
 
+		TEST_METHOD(TestMethod4)
+		{
+			Assert::AreEqual({ 1,11,55,165,330,462,462,330,165,55,11,1 }, GenerateRow(11));
+		}
+
 		vector<int> GetNextRow(vector<int> row, int i, vector<int> nextRow = {})
 		{
-			(i == 0 || i == row.size()) ? nextRow.emplace_back(1) : nextRow.emplace_back(row[i - 1] + row[i]);
+			i == 0 || i == row.size() ? nextRow.emplace_back(1) : nextRow.emplace_back(row[i - 1] + row[i]);
 			return i==0 ? nextRow : GetNextRow(row, i-1, nextRow);
 		}
 
