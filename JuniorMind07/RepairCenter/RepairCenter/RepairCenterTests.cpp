cea37c112c36e552d870cbc9bddc7d01239eed26
brankoEU <branko.tudorovici@gmail.com>
2017-10-25 15:19:54 +0300

    Repair Center New Proj

---
+++ JuniorMind07/RepairCenter/RepairCenter/RepairCenterTests.cpp
@@ -0,0 +1,18 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace RepairCenter
+{		
+	TEST_CLASS(SortingTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			// TODO: Your test code here
+		}
+
+	};
+}
