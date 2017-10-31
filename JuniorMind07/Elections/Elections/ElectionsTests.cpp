6c9f275f42f1cc5e29295f6a9ff15118dc93b1c6
brankoEU <branko.tudorovici@gmail.com>
2017-10-31 10:50:33 +0200

    Elections Sort New Proj

---
+++ JuniorMind07/Elections/Elections/ElectionsTests.cpp
@@ -0,0 +1,20 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include <vector>
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace std;
+
+namespace Elections
+{		
+	TEST_CLASS(ElectionsSortTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			// TODO: Your test code here
+		}
+
+	};
+}
